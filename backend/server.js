f4a46305963bc4fb3d914350a39e2dc3ec4d1c32
Priyanshu garg <gpriyanshu585@gmail.com>
2025-09-11 14:20:03 +0530

    dog apis error resolved

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ import employeeRoutes from './routes/employeeRoutes.js';
 app.use('/api/employees', employeeRoutes)
 
 import dogRoutes from './routes/dogRoutes.js';
-app.use('/api/dog-adoption', dogRoutes)
+app.use('/api/dogs', dogRoutes)
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
